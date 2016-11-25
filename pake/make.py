4b1e5c6a5507be261ea47a6fce66cb84d129af34
Teriks <Teriks@users.noreply.github.com>
2016-11-25 05:46:26 -0600

    pmake -> pake in license header

--- pake/make.py
+++ pake/make.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2016, Teriks
 # All rights reserved.
 #
-# pmake is distributed under the following BSD 3-Clause License
+# pake is distributed under the following BSD 3-Clause License
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 #
