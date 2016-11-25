1f8923cfe243dc78854fe6932382875c8660fe02
Teriks <Teriks@users.noreply.github.com>
2016-11-25 04:51:32 -0600

    Validate -j/--jobs gt one

--- pake/program.py
+++ pake/program.py
@@ -25,11 +25,19 @@ import sys
 
 _arg_parser = argparse.ArgumentParser()
 
+
+def _validate_gt_one(value):
+    i_value = int(value)
+    if i_value < 1:
+        _arg_parser.error("Number of jobs cannot be less than 1.")
+    return i_value
+
+
 _arg_parser.add_argument("targets", type=str, nargs='+', help="Build targets.")
 
 _arg_parser.add_argument("-j", "--jobs",
                          metavar="NUM_JOBS",
-                         type=int,
+                         type=_validate_gt_one,
                          default=1,
                          help="Max number of jobs, default is 1.")
 
