104230d741671fee78541c1255034595fb5f3980
Teriks <Teriks@users.noreply.github.com>
2017-05-01 00:27:20 -0500

    Slight amendment to documentation

--- pake/pake.py
+++ pake/pake.py
@@ -68,7 +68,10 @@ class TaskContext:
 
     @property
     def io(self):
-        """Task IO file stream."""
+        """Task IO file stream, a file like object that is only open for writing during a tasks execution.
+        
+        Any output to be displayed for the task should be writen to this file object.
+        """
         return self._io
 
     def print(self, *args, **kwargs):
