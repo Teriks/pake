c3417b807476adb547d21c68a7f71cc81950a62d
Teriks <Teriks@users.noreply.github.com>
2017-06-14 23:07:04 -0500

    Bump revision for readme edit

--- pake/__init__.py
+++ pake/__init__.py
@@ -21,7 +21,7 @@
 __author__ = 'Teriks'
 __copyright__ = 'Copyright (c) 2016 Teriks'
 __license__ = 'Three Clause BSD'
-__version__ = '0.12.0.0'
+__version__ = '0.12.0.1'
 
 from .filehelper import FileHelper
 
