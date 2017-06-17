f6d3ec34bdcebddfb06bc3c1a8ed31ab6a5ee0bd
Teriks <Teriks@users.noreply.github.com>
2017-06-16 20:48:21 -0500

    Change the exception message of TaskExitException
    Clarify the mentioned number is the return code

--- pake/pake.py
+++ pake/pake.py
@@ -127,7 +127,7 @@ class TaskExitException(Exception):
         :param task_name: The name of the task that raised the :py:exc:`SystemExit` exception.
         :param exception: Reference to the :py:exc:`SystemExit` or derived exception raised inside the task.
         """
-        super().__init__('Exit exception {cls}({code}) was raised within task "{task}".'
+        super().__init__('Exit exception {cls}() was raised within task "{task}".  Return code: {code}'
                          .format(cls=pake.util.qualified_name(exception),
                                  code=exception.code,
                                  task=task_name))
