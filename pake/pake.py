3c1bc9d005bf2fc04c8a7b074db606888158de39
Teriks <Teriks@users.noreply.github.com>
2017-06-20 01:54:48 -0500

    Amend search replace error in comment

--- pake/pake.py
+++ pake/pake.py
@@ -1701,7 +1701,7 @@ class Pake:
             finally:
                 # Be very paranoid about object state
 
-                t_pool = self._threadpool  # t_pool de_init might throw, maybe
+                t_pool = self._threadpool  # t_pool.shutdown might throw, maybe
 
                 # Fix object state
                 self._threadpool = None
